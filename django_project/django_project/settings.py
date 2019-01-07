de62788214b6f11e0652eb4d324534e8bf22fc13
HericSaurus <bch_heric@hotmail.com>
2019-01-07 23:28:57 +0800

    step 1 finish

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #add app from blog
+    'blog.apps.BlogConfig',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 MIDDLEWARE = [
