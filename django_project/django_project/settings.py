179698207480e38e2aa8925a04770864fd5f8fe1
HericSaurus <bch_heric@hotmail.com>
2019-01-09 22:23:38 +0800

    done !!

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#IA for media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 #IA add for Crispy
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
